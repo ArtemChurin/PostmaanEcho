d8830f83ceae713fd7c5f3ff9d07b8a2dc496142
ArtemChurin <artem.churin.1976@gmail.com>
2022-07-22 17:49:59 +0300

    fix

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -16,7 +16,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("драсьте! Приехали! Вылазьте!"))
+                .body("data", equalTo("Здрасьте! Приехали! Вылазьте!"))
         ;
     }
 }
